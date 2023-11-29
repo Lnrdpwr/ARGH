ac0bd041cee0431e68570230b0fc1394ba63a97e
Lnrdpwr <98341880+Lnrdpwr@users.noreply.github.com>
2023-11-29 14:01:26 +0500

    Add files via upload

---
+++ PlayerHealth.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] private Transform _checkpoint;
+    [SerializeField] private GameObject _deathEffect;
+    [SerializeField] private float _resurrectionTime;
+
+    public void DoDamage()
+    {
+        Instantiate(_deathEffect, transform.position, Quaternion.identity);
+        gameObject.SetActive(false);
+        StartCoroutine(Resurrection());
+    }
+
+    IEnumerator Resurrection()
+    {
+        yield return new WaitForSeconds(_resurrectionTime);
+        transform.position = _checkpoint.position;
+        gameObject.SetActive(true);
+    }
+}
