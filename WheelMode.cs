5ced949832894f557535a79c85f4fa2c3ee514f8
Lnrdpwr <98341880+Lnrdpwr@users.noreply.github.com>
2023-11-30 12:43:53 +0500

    Add files via upload

---
+++ WheelMode.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class WheelMode : MonoBeahviour
+{
+    [Header("Параметры колеса")]
+    [SerializeField] private float _maxAcceleration;
+    [SerializeField] private float _accelerationGain;
+    private float _acceleration;
+
+    private Rigidbody2D _rigidbody;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        _acceleration = Mathf.Clamp(-_maxAcceleration, _maxAcceleration, _acceleration + horizontalInput * Time.deltaTime * _accelerationGain);
+        _rigidbody.velocity = new Vector2(_acceleration, _rigidbody.velocity.y);
+    }
+}
